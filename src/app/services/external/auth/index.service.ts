f44af57e6d058d1e7101fd41a099ce1d484d1846
Henrique Barros <henriquebarrosx@gmail.com>
2024-08-18 15:21:37 -0300

    Correção dos endpoints para gerenciamento da sessão

--- src/app/services/external/auth/index.service.ts
+++ src/app/services/external/auth/index.service.ts
@@ -13,17 +13,17 @@ export class AuthService {
   constructor(private httpClient: HttpClient) { }
 
   signIn(payload: SignInRequestParamsDTO): Observable<SignInResponseDTO> {
-    return this.httpClient.post<SignInResponseDTO>(`${environment.API_BASE_URL}/api/auth/sign-in`, payload)
+    return this.httpClient.post<SignInResponseDTO>(`${environment.API_BASE_URL}/v1/auth/sign-in`, payload)
       .pipe(take(1));
   }
 
   signUp(payload: SignUpRequestParamsDTO): Observable<{ id: number }> {
-    return this.httpClient.post<{ id: number }>(`${environment.API_BASE_URL}/api/auth/sign-up`, payload)
+    return this.httpClient.post<{ id: number }>(`${environment.API_BASE_URL}/v1/auth/sign-up`, payload)
       .pipe(take(1));
   }
 
   signOut(): Observable<void> {
-    return this.httpClient.delete<void>(`${environment.API_BASE_URL}/api/auth/sign-out`)
+    return this.httpClient.delete<void>(`${environment.API_BASE_URL}/v1/auth/sign-out`)
       .pipe(take(1));
   }
 }
