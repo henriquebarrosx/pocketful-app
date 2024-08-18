30fcef45219cb5d357265657d66bc4052e49a11f
Henrique Barros <henriquebarrosx@gmail.com>
2024-08-18 15:21:09 -0300

    Correção da observabilidade da stream de dados da sessão em cache

--- src/app/components/header-bar/header-bar.component.ts
+++ src/app/components/header-bar/header-bar.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 
+import { AuthService } from '../../services/external/auth/index.service';
 import { SessionService } from '../../services/internal/session/index.service';
 import { ButtonComponent } from '../button/button.component';
 import { SimpleDialogComponent } from '../simple-dialog/index.component';
@@ -15,16 +15,26 @@ import { SimpleDialogComponent } from '../simple-dialog/index.component';
 })
 export class HeaderBarComponent {
   isDialogVisible: boolean = false
-  isAuthenticated$: Observable<boolean>
-  signedAccountName: string
+  isAuthenticated: boolean = false
+  sessionName: string = ''
 
-  constructor(private sessionService: SessionService) {
-    this.isAuthenticated$ = this.sessionService.isAuthenticated
-    this.signedAccountName = this.sessionService.get()?.name ?? ''
+  constructor(
+    private sessionService: SessionService,
+    private authService: AuthService,
+  ) {
+    this.sessionService.session.subscribe(session => {
+      this.isAuthenticated = !!session
+      this.sessionName = session?.name ?? ''
+    })
   }
 
   logout() {
-    this.sessionService.destroy()
+    this.authService.signOut().subscribe({
+      next: () => {
+        this.closeDialog()
+        this.sessionService.destroy()
+      }
+    })
   }
 
   openDialog() {
